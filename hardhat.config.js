48fba08d1ba922779adf4f12b88b013118d4da34
yulucket <lucket@outlook.com>
2022-01-19 11:44:30 +0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: "1UNJUQGAYE62RQ3B7CX7EQNYJTJ1TMSVCQ",
+  }
+};
